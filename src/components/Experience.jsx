7654f5e9aba1badfa8dda556a89d17957fe0e05d
Jatin Thakur <1000071072@hexaware.com>
2023-06-02 16:20:43 +0530

    plane

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Float } from "@react-three/drei";
 import { Background } from "./Background";
 import { Airplane } from "./Airplane";
 import { Cloud } from "@react-three/drei";
@@ -7,12 +7,15 @@ export const Experience = () => {
     <>
       <OrbitControls />
       <Background />
-      <Airplane />
+      <Float floatIntensity={2} speed={2}>
+        <Airplane
+          rotation-y={Math.PI / 2}
+          scale={[0.2, 0.2, 0.2]}
+          position-y={0.1}
+        />
+      </Float>
+
       <Cloud />
-      <mesh>
-        {/* <boxGeometry />
-        <meshNormalMaterial /> */}
-      </mesh>{" "}
     </>
   );
 };
