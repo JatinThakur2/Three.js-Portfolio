07ef8780fbc0a2a3c4b3ed33cc78fef8578bfd24
Jatin Thakur <1000071072@hexaware.com>
2023-06-02 15:48:15 +0530

    added clouds and Plane

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,28 +4,25 @@ import { Gradient, LayerMaterial } from "lamina";
 import * as THREE from "three";
 
 export const Background = () => {
-    return ( <
+  return (
+    <>
+      <Environment preset="sunset" />
+      <Sphere scale={[100, 100, 100]} rotation-y={Math.PI / 2}>
+        <LayerMaterial
+          color={"#ffffff"}
+          lighting="physical"
+          transmission={1}
+          side={THREE.BackSide}
         >
-        <
-        Environment preset = "sunset" / >
-        <
-        Sphere scale = {
-            [100, 100, 100]
-        }
-        rotation - y = { Math.PI / 2 } >
-        <
-        LayerMaterial color = { "#ffffff" }
-        lighting = "physical"
-        transmission = { 1 }
-        side = { THREE.BackSide } >
-        <
-        Gradient colorA = { "#357ca1" }
-        colorB = { "white" }
-        axes = { "y" }
-        start = { 0 }
-        end = {-0.5 }
-        /> < /
-        LayerMaterial > { " " } <
-        /Sphere>{" "} < / >
-    );
+          <Gradient
+            colorA={"#357ca1"}
+            colorB={"white"}
+            axes={"y"}
+            start={0}
+            end={-0.5}
+          />
+        </LayerMaterial>{" "}
+      </Sphere>{" "}
+    </>
+  );
 };
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,17 @@
 import { OrbitControls } from "@react-three/drei";
 import { Background } from "./Background";
+import { Airplane } from "./Airplane";
+import { Cloud } from "@react-three/drei";
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
       <Background />
+      <Airplane />
+      <Cloud />
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        {/* <boxGeometry />
+        <meshNormalMaterial /> */}
       </mesh>{" "}
     </>
   );
